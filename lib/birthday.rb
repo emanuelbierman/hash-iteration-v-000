acb130b6e16406abc2d78479c343038e7647afec
Emanuel Bierman <emanuel.bierman@gmail.com>
2018-05-08 00:40:52 +0000

    automatically backed up by learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -5,5 +5,7 @@
 # }
 
 def happy_birthday(birthday_kids)
-  birthday_kids.each {|kid| puts kid}
+  birthday_kids.each do |key, value|
+    puts "Happy Birthday #{birthday_kids[value]}! You are now #{birthday_kids[key]} years old!"
+  end
 end
